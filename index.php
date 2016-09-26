82868cc970cd5cff7afbf5b0a6cf5b4874823897
Esanalok <esanalok@gmail.com>
2016-09-26 20:05:37 +0600

    New modify

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
-<?php
-    echo "Hello Git";
-    echo "<br/>";
+<html>
+    <head>
+        <title>Use of Git</title>
+        <style>
+            .text{
+                color : red;
+                font-size: 3em;
+            }
 
-?>
+        </style>
+    </head>
+    <body>
+        <h1 class="text">
+            <?php
+            echo "Hello Git";
+            echo "<br/>";
+            echo "This is my first git project";
+            ?>
+        </h1>
+    </body>
+</html>
